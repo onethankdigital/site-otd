0ab61e00070820c57ecc300f856233723955aeb2
Robson LQL <onethank.mkt@gmail.com>
2026-06-02 14:52:39 -0300

    build: adiciona vite-plugin-sitemap e configuração de sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
+import { blogPosts } from './src/data/blogPosts.js'
+
+const paths = [
+  '/diagnostico',
+  '/quiz',
+  '/pilares',
+  '/4-pilares',
+  '/historia',
+  '/nossa-historia',
+  '/blog',
+  '/cases',
+  '/privacidade',
+  '/termos',
+  ...blogPosts.map(post => `/blog/${post.slug}`)
+]
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://www.onethank.com.br',
+      dynamicRoutes: paths
+    })
+  ],
   build: {
     modulePreload: false
   }
 })
+
